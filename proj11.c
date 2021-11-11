5a8d7733e9e7c2f62642d1a6242615c44562cf81
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-11 14:13:51 +0100

    ajout

---
+++ proj11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  float l;
+  float L;
+  printf("donner le largeur: \n");
+  scanf("%f",&l);
+  printf("donner la langueur: \n");
+  scanf("%f",&L);
+   printf("circonférence  : %f\n", 2 * (l + L));
+
+
+
+
+    return 0;
+}
