2514241eabf9fd754482d9b92e66f0617c588041
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-11 15:18:53 +0100

    ajout

---
+++ proj12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void convert(int n);
+printf("%d" ,n%10);
+if (n>10){
+    convert(n/10);
+}
+
+
+
+int main()
+{
+    int n;
+
+    printf("entrer Nombre :");
+    scanf("%d", &n);
+    convert(n);
+
+
+    return 0;
+}
