5571cc5a10cd3cf0d69bc2f9234a0d21152d3c40
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-11 16:29:56 +0100

    ajout

---
+++ proj13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  long int N ;
+printf("saisir un entier");
+scanf("%d",&N);
+
+printf("en hexadecimal : %x\n" ,N);
+printf("en octel : %o" ,N);
+
+    return 0;
+
+}
