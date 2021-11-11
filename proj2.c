b044ba6cb237b8fe4fafb4d9bc731e0bdbd9e3c0
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-11 14:27:00 +0100

    ajout

--- proj2.c
+++ proj2.c
@@ -1,32 +1,17 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
+int main()
+{
 
-    char nom[20];
-    char prenom[20];
-    char age[20];
-    char sexe[20];
-    char tele[20];
-
-    printf("Votre Nom\n");
-    scanf("%s", nom);
-
-    printf("Votre Prenom\n");
-    scanf("%s", prenom);
-
-    printf("Votre Age\n");
-    scanf("%s", age);
-
-    printf("Votre Sexe\n");
-    scanf("%s", sexe);
-
-    printf("Votre Tele\n");
-    scanf("%s", tele);
+float f ,c;
 
 
-    printf("Nom Complet : %s %s, Age : %s, Sexe : %s, Tele : %s\n", nom, prenom, age, sexe, tele);
+    printf(" température en Celsius : \n " );
+    scanf("%f",&f);
+    f = (c * 1.8) + 32;
+        printf(" temperature en Fahrenheit  est %.2f F : \n ",f );
+
 
     return 0;
-
 }
